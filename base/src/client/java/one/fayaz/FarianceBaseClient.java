8392c13ae599d11ad0f7cc23b5f31167bd7c9a26
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2025-08-06 18:13:07 -0400

    create mods

---
+++ base/src/client/java/one/fayaz/FarianceBaseClient.java
@@ -0,0 +1,10 @@
+package one.fayaz;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FarianceBaseClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
